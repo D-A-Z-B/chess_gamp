eaa329820452fd1f017836f0ef5cf203c72b67f0
DAZB <jiwoo2426p@gmail.com>
2024-11-29 23:49:13 +0900

    king attack

--- 2024_winapigamep_framework/KingAttackObject.cpp
+++ 2024_winapigamep_framework/KingAttackObject.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 #include "KingAttackObject.h"
 
+#include "EventManager.h"
+#include "TimeManager.h"
+
 KingAttackObject::KingAttackObject()
 {
 }
@@ -38,9 +41,34 @@ void KingAttackObject::ExitCollision(Collider* _other)
 
 void KingAttackObject::StartAttackRoutine(Vec2 size, float duration)
 {
+	isAttack = true;
+	targetSize = size;
+	targerDuration = duration;
 }
 
 void KingAttackObject::AttackRoutine()
 {
-	
+	static float elapsedTime = 0;
+
+	Vec2 startSize = {0, 0};
+	Vec2 targetSize = this->targetSize;
+
+	if (elapsedTime < targerDuration) {
+		float t = elapsedTime / targerDuration;
+		float calcT = t == 0 ? 0 : t == 1 ? 1 : t < 0.5 ? pow(2, 20 * t - 10) / 2 : (2 - pow(2, -20 * t + 10)) / 2;
+
+		float x = startSize.x * (1 - calcT) + targetSize.x * calcT;
+		float y = startSize.y * (1 - calcT) + targetSize.y * calcT;
+
+		SetSize({ x, y });
+
+		cout << x << endl;
+
+		elapsedTime += fDT;
+	}
+	else {
+		GET_SINGLE(EventManager)->DeleteObject(this);
+
+		elapsedTime = 0;
+	}
 }
